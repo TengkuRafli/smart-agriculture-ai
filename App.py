8d519139f74ed2509424a817735eb18c0162f7b1
TengkuRafli <t.rafli@students.polmed.ac.id>
2026-04-18 07:06:48 +0700

    Update App.py

--- App.py
+++ App.py
@@ -71,8 +71,5 @@ def api_predict():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
